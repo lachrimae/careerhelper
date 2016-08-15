0558389d67e60988ba69364a0c6a129be1fb4eb8
Curran McConnell <curran.mcconnell@protonmail.com>
2016-08-15 02:06:34 +0000

    URL /app/ resolves!

--- careerhelper/careerapp/views.py
+++ careerhelper/careerapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+#from .forms import CareerForm
 
 # Create your views here.
+
+def homepage_redirect(request):
+    return HttpResponseRedirect("/app/")
+
+def homepage(request):
+    return HttpResponse("Hello there. You've found the recommender page.")
+
+#def homepage(request):
+    """
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/results/')
+    else:
+        form = NameForm()
+    return render(request, 'homepage.html', {'form': form})
+    """
+#    return django.http.HttpResponse("Hello there. You've found the recommender page.")
---
+++ careerhelper/startserver.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /home/admin/envs/careerhelper/bin/activate
+authbind gunicorn --bind 0.0.0.0:80 careerhelper.wsgi:application &
