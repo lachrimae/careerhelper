f31a11535a5cd55658da552475fd840c3302472d
Curran McConnell <curran.mcconnell@protonmail.com>
2016-08-16 20:39:59 +0000

    Setup models for Raw data

--- careerhelper/careerapp/models.py
+++ careerhelper/careerapp/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+class Scale(models.Model):
+    code = models.CharField(primary_key=True)
+    name = models.CharField()
+    min = models.IntegerField()
+    max = models.IntegerField()
+
+class JobRaw(models.Model):
+    code = models.CharField(primary_key=True)
+    title = models.CharField()
+    description = models.CharField()
+
+class EducationRaw(models.Model):
+    code = models.CharField(primary_key=True)
+    title = models.CharField()
+    description = models.CharField()
+    crosscode = ??????
+
+class WorkerQuality(models.Model):
+    code = models.CharField(primary_key=True)
+    name = models.CharField()
+    description = models.CharField()
+
+class JobRating(models.Model):
+    job = models.ForeignKey('Job')
+    quality = models.ForeignKey('WorkerQuality')
+    scale = models.ForeignKey('Scale')
+    value = models.FloatField()
+    n = models.IntegerField()
+    lowerbound = models.FloatField()
+    upperbound = models.FloatField()
+    suppress = models.BooleanField()
+    irrelevant = models.BooleanField()
+    date = models.CharField()
+    source = models.CharField()
+
+class MindsumoSample(models.Model):
+    name = models.CharField(primary_key=True)
+    
+class MindsumoCellRaw(models.Model):
+    jobcode = models.ForeignKey('Job')
+    sample = models.CharField()
+    education = models.ForeignKey('Education')
+    n = models.ForeignKey('MindsumoSample') 
+
+class JobRelation(models.Model):
+    job = models.ForeignKey('Job')
+    relatedJob = models.ForeignKey('Job')
